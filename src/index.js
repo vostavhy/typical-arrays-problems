4ce94df9bbe5b9d5925f49856ba954ad77a220d0
vostavhy <vostavhy@gmail.com>
2021-07-25 16:12:44 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((array == undefined) || (array.length == 0)) return 0;
+
+  let min = Infinity;
+
+  for (let i of array) {
+      if (i < min) min = i;
+  }
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((array == undefined) || (array.length == 0)) return 0;
+
+    let max = -Infinity;
+
+    for (let i of array) {
+        if (i > max) max = i;
+    }
+
+  return max;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((array == undefined) || (array.length == 0)) return 0;
+
+    let sum = 0;
+
+    for (let i of array) {
+        sum += i;
+    }
+
+    return (sum / array.length)
 }
